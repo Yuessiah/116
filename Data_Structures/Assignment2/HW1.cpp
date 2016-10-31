bc9bf28d7605aa134e2650092c9d03c937c275da
Yuessiah <yuessiah@gmail.com>
2016-11-01 06:44:43 +0800

    (´-ω-｀)

---
+++ Data_Structures/Assignment2/HW1.cpp
@@ -0,0 +1,44 @@
+#include<stdio.h>
+const int maxn = 100;
+
+struct matrix_1_dimension {
+	int value;
+	int i, j;
+}e[maxn*maxn];
+int n, D[maxn][maxn], a, b;
+
+void input()
+{
+	scanf("%d", &n);
+	for(int i = 0; i < n; i++)
+		for(int j = 0; j < n; j++)
+			scanf("%d", &D[i][j]);
+	printf("a: ");
+	scanf("%d", &a);
+	printf("b: ");
+	scanf("%d", &b);
+}
+
+int main()
+{
+	input();
+
+	int eLen = 0;
+	for(int i = 0; i < n; i++) {
+		int jEnd = (b-1)-1+i+1;
+		if(jEnd >= n) jEnd = n-1;
+		for(int j = (i<=(a-1))? 0 : i-(a-1); j <= jEnd; j++) {
+			e[eLen].value = D[i][j];
+			e[eLen].i = i; e[eLen].j = j;
+			eLen++;
+		}
+	}
+
+	for(int i = 0; i < eLen; i++) {
+		char buf[12];
+		sprintf(buf, "e[%d]=%d", i, e[i].value);
+		printf("%-11sd(%d,%d)\n", buf, e[i].i, e[i].j);
+	}
+
+	return 0;
+}
