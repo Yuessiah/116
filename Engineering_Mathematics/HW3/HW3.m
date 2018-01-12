b67d341a0b5f80da2ef81ceb4a33f61ca5445fbb
yuessiah <yuessiah@gmail.com>
2018-01-12 17:03:50 +0900

    ( ^ω^)

--- Engineering_Mathematics/HW3/HW3.m
+++ Engineering_Mathematics/HW3/HW3.m
@@ -24,17 +24,15 @@ hold off;
 %%
 
 h = 0.1;
-v = sym('x');
-t = sym('y');
-f = 2*v - 3*t + 1;
-g = t + h*f; % euler's method
+syms v t;
+f = symfun(2*v - 3*t + 1, [v, t]);
+e = t + h*f; % euler's method
 
-ans = zeros(10, 2);
-yn = 5;
+y = zeros(1, 11);
 x = [1:0.1:2];
+y(1) = 5;
 for n = 1:1:10
-    ans(n,:) = [double(x(n)), double(yn)];
-    yn = subs(g, [v, t], [x(n), yn]); % calculate y_{n+1}
+    y(n+1) = subs(e, [v, t], [x(n), y(n)]); % calculate y_{n+1}
 end
 
-disp(ans);
+disp([x', y']);
